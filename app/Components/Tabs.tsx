8c625023438de6ffd8224b8f223ee6b42b5804ae
Bas-sheikh <basilelshakhe@gmail.com>
2023-07-17 20:55:34 +0300

    handled tabs component properties like position, size, and centered relates #7

--- app/Components/Tabs.tsx
+++ app/Components/Tabs.tsx
@@ -4,16 +4,26 @@ import TabButton from "./TabButton";
 
 interface ItemsProps {
   key: string;
-  label: string;
+  label: string | React.ReactElement;
+  disabled?: boolean;
   children?: string | React.ReactElement;
 }
 
 interface TabsProps {
   items: ItemsProps[];
+  centered?: boolean;
+  size?: string;
+  position?: string;
   defaultActiveKey?: string;
 }
 
-const Tabs: React.FC<TabsProps> = ({ items, defaultActiveKey }) => {
+const Tabs: React.FC<TabsProps> = ({
+  items,
+  defaultActiveKey,
+  size,
+  position,
+  centered,
+}) => {
   const [active, setActive] = useState(defaultActiveKey);
 
   const handleTabClick = (key: string) => {
@@ -21,25 +31,51 @@ const Tabs: React.FC<TabsProps> = ({ items, defaultActiveKey }) => {
   };
 
   return (
-    <>
-      <div className="flex w-full items-center gap-4 border-b border-b-[#cccccc]">
+    <div
+      className={`
+        flex w-full gap-4 flex-col
+        ${position === "bottom" && "flex-col-reverse"}
+        ${position === "left" && "!flex-row"}
+        ${position === "right" && "!flex-row-reverse"}
+    `}
+    >
+      <div
+        className={`
+          flex gap-4 border-b border-b-[#cccccc] w-full overflow-auto scroll-smooth
+          ${position === "bottom" && "border-t border-t-[#cccccc] border-b-0"}
+          ${
+            position === "left" &&
+            "border-r border-r-[#cccccc] flex-col border-b-0 !w-[150px] h-[600px]"
+          }
+          ${
+            position === "right" &&
+            "border-l border-l-[#cccccc] flex-col border-b-0 !w-[150px] h-[600px]"
+          }
+          ${
+            centered && "justify-center"
+          }
+      `}
+      >
         {items?.map((item) => (
           <TabButton
             isActive={item.key === active ? true : false}
             key={item.key}
             keyElement={item.key}
+            size={size || "small"}
+            position={position || "top"}
+            disabled={item.disabled || false}
             onClick={() => handleTabClick(item.key)}
             label={item.label}
           />
         ))}
       </div>
-      <div className="flex w-full items-center gap-8 mt-8">
+      <div className="flex w-[90%] gap-8">
         {items?.map((item) => {
           if (item.key !== active) return;
           return <div key={item.key}>{item.children}</div>;
         })}
       </div>
-    </>
+    </div>
   );
 };
 
